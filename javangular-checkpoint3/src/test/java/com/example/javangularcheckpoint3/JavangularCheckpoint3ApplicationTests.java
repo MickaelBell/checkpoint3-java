d519f3daf70c4444b8fab0385607d6b171f6cebb
mickaelBell <mickael.belleuvre@gmail.com>
2021-01-08 09:37:49 +0100

    add spring project

---
+++ javangular-checkpoint3/src/test/java/com/example/javangularcheckpoint3/JavangularCheckpoint3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javangularcheckpoint3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavangularCheckpoint3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
