7079d415ab21da4baf7bdff39bf969622886f5ab
mickaelBell <mickael.belleuvre@gmail.com>
2021-01-09 19:48:36 +0100

    add join post to user

--- cp3/src/main/java/com/checkpoint3/cp3/entity/Post.java
+++ cp3/src/main/java/com/checkpoint3/cp3/entity/Post.java
@@ -2,10 +2,15 @@ package com.checkpoint3.cp3.entity;
 
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 
 
@@ -19,6 +24,11 @@ public class Post {
 	private long id;
 	private String message;
 	private String imageUrl;
+	
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+    User user;
 
 	public Post() {
 	}
